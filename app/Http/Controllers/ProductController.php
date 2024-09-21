2a5cb322b0e48210cc3f9c9e5ffb67f0381030ed
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2024-09-21 09:39:37 +0800

    fix: Update product edit functionality, but still unable to delete old image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 // import model product
 use App\Models\Product;
-
+use Illuminate\Contracts\Cache\Store;
 // import return type view
 use illuminate\View\View;
+
+// import facedes storage
+use Illuminate\Support\Facades\Storage;
+
 
 // import return type RedirectResponse
 use illuminate\Http\RedirectResponse;
@@ -88,17 +92,74 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        // get product by id
+        $product = Product::findOrFail($id);
+
+        // render view with products
+        return view('products.edit', compact('product'));
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id) : RedirectResponse
     {
-        //
+        //validate form
+        $request->validate([
+            'image'         => 'nullable|image|mimes:jpeg,jpg,png|max:2048', // Ganti 'required' dengan 'nullable'
+            'title'         => 'required|min:5',
+            'description'   => 'required|min:10',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric'
+        ]);
+
+        // get product by id.
+        $product = Product::findOrFail($id);
+
+        // check if image is uploaded
+        if ($request->hasFile('image')) {
+            
+            // upload new image
+            $image = $request->file('image');
+            // $image->storeAs('public/products', $image->hashName());
+            // upload image ke disk 'public'
+            $image->storeAs('products', $image->hashName(), 'public');
+
+            // delete old image
+            // Storage::delete('products'.$product->image, 'public');
+            Storage::delete('products/' . $product->image);
+            // Delete old image
+        // if ($product->image && Storage::exists('products/' . $product->image)) {
+        //     Storage::delete('products/' . $product->image);
+        // }
+
+
+            // update product with new image
+            $product->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+
+            
+        } else {
+            
+            // update product without image
+            $product->update([
+                'title'         => $request->title,
+                'description'   => $request->description,
+                'price'         => $request->price,
+                'stock'         => $request->stock
+            ]);
+        }
+
+        // redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Berhasi Diubah']);
     }
 
     /**
